999ae3cfba648e6966c4512e10b64c6463a2db1b
yoneya <Yoneya.ohm1221@gmail.com>
2022-03-03 12:34:08 +0700

    update

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -78,3 +78,12 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+//calculate age
+var dob = new Date("08/17/1999");  
+var month_diff = Date.now() - dob.getTime();   
+var age_dt = new Date(month_diff);      
+var year = age_dt.getUTCFullYear();  
+var age = Math.abs(year - 1970);  
+document.getElementById("myage").innerHTML =String(age);
+
